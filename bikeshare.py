a80c6c713f24f696efd180d35bda0612d4ad0822
Daniela Renedo <dadarenedo@gmail.com>
2020-12-12 15:23:45 -0500

    fix, <I added  the missing function>

--- bikeshare.py
+++ bikeshare.py
@@ -226,19 +226,19 @@ def display_data(df):
     
     
 def main():
-    while True:
-        city, month, day = get_filters()
-        df = load_data(city, month, day)
+while True:
+    city, month, day = get_filters()
+    df = load_data(city, month, day)
 
-        time_stats(df)
-        station_stats(df)
-        trip_duration_stats(df)
-        user_stats(df)
-        display_data(df)   """ dont forget to add this function here! """
+    time_stats(df)
+    station_stats(df)
+    trip_duration_stats(df)
+    user_stats(df)
+    display_data(df)   """this fuction was missing """
 
-        restart = input('\nWould you like to restart? Enter yes or no.\n')
-        if restart.lower() != 'yes':
-            break
+    restart = input('\nWould you like to restart? Enter yes or no.\n')
+    if restart.lower() != 'yes':
+        break
 
 
 if __name__ == "__main__":
